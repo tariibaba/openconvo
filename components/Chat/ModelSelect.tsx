44f336adfd4ee0e6fb65661accd0b6100e1a1029
Ayibatari Ibaba <tariibabadev@gmail.com>
2023-06-12 22:18:16 +0100

    fix: Pass 3rd argument

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -11,26 +11,26 @@ export const ModelSelect = () => {
   const { t } = useTranslation('chat');
 
   const {
-    state: { selectedConversation, models, defaultModelId },
+    state: { selectedConversation, models, defaultModelId, conversations },
     handleUpdateConversation,
     dispatch: homeDispatch,
   } = useContext(HomeContext);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     selectedConversation &&
-      handleUpdateConversation(selectedConversation, {
-        key: 'model',
-        value: models.find(
-          (model) => model.id === e.target.value,
-        ) as OpenAIModel,
-      });
+      handleUpdateConversation(
+        selectedConversation,
+        {
+          key: 'model',
+          value: models.find((model) => model.id === e.target.value) as OpenAIModel,
+        },
+        conversations,
+      );
   };
 
   return (
     <div className="flex flex-col">
-      <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('Model')}
-      </label>
+      <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">{t('Model')}</label>
       <div className="w-full rounded-lg border border-neutral-200 bg-transparent pr-2 text-neutral-900 dark:border-neutral-600 dark:text-white">
         <select
           className="w-full bg-transparent p-2"
@@ -39,14 +39,8 @@ export const ModelSelect = () => {
           onChange={handleChange}
         >
           {models.map((model) => (
-            <option
-              key={model.id}
-              value={model.id}
-              className="dark:bg-[#343541] dark:text-white"
-            >
-              {model.id === defaultModelId
-                ? `Default (${model.name})`
-                : model.name}
+            <option key={model.id} value={model.id} className="dark:bg-[#343541] dark:text-white">
+              {model.id === defaultModelId ? `Default (${model.name})` : model.name}
             </option>
           ))}
         </select>
